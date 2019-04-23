607cb6c06be57e4ea5150c2b6d046c925a9d2ffb
feven <feventsegay6@gmail.com>
2019-04-23 02:21:37 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -26,7 +26,7 @@ def third_challenge
         },
       :heroine => {
         #your key/value pairs here
-        
+        name: "Juliet", age: "15", status: "alive"
         },
       :heroine_friends => []
    }
