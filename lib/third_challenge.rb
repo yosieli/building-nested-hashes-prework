e8d61aa8a546c13accad26909b51770a224bf689
feven <feventsegay6@gmail.com>
2019-04-23 02:21:03 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -18,6 +18,7 @@ def third_challenge
    :capulet => {
       :patriarch => {
         #your key/value pairs here
+        name: "Lord Capulet", age: "50"
         },
       :matriarch => {
         #your key/value pairs here
