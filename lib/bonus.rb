fce4c6be5d8e9b75287c4ea71e0be27b0863f2e9
feven <feventsegay6@gmail.com>
2019-04-23 02:39:17 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -24,7 +24,7 @@ def bonus
 
 epic_tragedy[:Montague][:hero][:status]="dead"
 epic_tragedy[:capulet][:heroine][:status]="dead"
-puts epic_tragedy
+epic_tragedy
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
 
