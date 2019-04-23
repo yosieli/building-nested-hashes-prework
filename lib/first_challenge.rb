cba95ca0bc68e894cf36fdd1b760edeb64b5e0b4
feven <feventsegay6@gmail.com>
2019-04-23 02:00:46 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -5,8 +5,15 @@ def first_challenge
       :name => "Lord Montague",
       :age => "53"
        },
-       
-     }
+       :matriarch => {
+      :name => "Lady Montague",
+      :age => "54"
+  },
+    :hero => {
+      :name => "Romeo",
+      :age => "15"
+     },
+     
      :capulet=>{
        
      }
